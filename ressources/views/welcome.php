874c0ee308dd51e7b03a3975ca05ea16211e0fac
DiD <rhandy.grard@gmail.com>
2015-12-15 14:45:38 +0100

    Fill homepage

--- ressources/views/welcome.php
+++ ressources/views/welcome.php
@@ -1,7 +1,30 @@
 <h3>Bievenue sur le site de GTA Démago</h3>
-<div>
-    GTA Démago est une émission diffusée régulièrement sur internet sur Twitch (<a href="http://www.twitch.tv/realmyop2">http://www.twitch.tv/realmyop2</a>)
-</div>
-<div>
-    Le mod GTA Démago peut être installé grâce aux launcher <a href="http://did.sytes.net/gtademago.rar">téléchargeable ici</a>. (Une version du jeu GTA V est nécessaire pour faire fonctionner le mod)
+<div class="row">
+    <div class="col s12">
+        <div class="card-panel">
+            GTA Démago est une émission diffusée régulièrement sur internet sur Twitch (<a href="http://www.twitch.tv/realmyop2">http://www.twitch.tv/realmyop2</a>)
+        </div>
+    </div>
+    <div class="col s12">
+        <div class="card-panel">
+            Le mod GTA Démago peut être installé grâce aux launcher <a href="http://did.sytes.net/gtademago.rar">téléchargeable ici</a>.
+            (Une version du jeu GTA V est nécessaire pour faire fonctionner le mod)
+        </div>
+    </div>
+    <div class="col center-align s12 m6">
+        <div class="card-panel">
+            Plusieurs platerformes de gestion des tâches sont disponibles pour chacun des projets lié à GTA Démago
+            <br/>
+            <a href="https://trello.com/gtademago">https://trello.com/gtademago</a>
+            <br/>
+            <img style="max-width:300px;" alt="Trello" src="./ressources/images/trello.png">
+        </div>
+    </div>
+    <div class="col center-align s12 m6">
+        <div class="card-panel">
+            Les différents projets sont hebergés sur GitHub.
+            <br/>
+            <img style="max-width:300px;" alt="Github" src="./ressources/images/github.png">
+        </div>
+    </div>
 </div>
