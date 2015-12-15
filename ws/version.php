5e56b63cabdb9d2084358f4cad07880db31eaddc
DiD <rhandy.grard@gmail.com>
2015-12-15 17:54:43 +0100

    Add download link in webservice for launcher

--- ws/version.php
+++ ws/version.php
@@ -2,16 +2,21 @@
 $response = array();
 $response['version'] = "0";
 $response['maxVersion'] = "0";
+$response['maxVersionDownloadLink'] = "";
 
 if (isset($_POST['checksum'])) {
+
     $pathToVersions = 'versions/';
     $files = scandir($pathToVersions);
     foreach ($files as $file) {
         if (strpos($file, ".dll") !== false) {
             $version = floatval(substr($file, 0, strpos($file, ".dll")));
 
-            if($version > $response['maxVersion'])
+            if ($version > $response['maxVersion']) {
                 $response['maxVersion'] = $version;
+                $response['maxVersionDownloadLink'] = 'http://did.sytes.net/projets/gta-demago/ws/files/mod/'.$version.'.zip';
+                $response['maxVersionTexturesLink'] = 'http://did.sytes.net/projets/gta-demago/ws/files/textures/'.$version.'.zip';
+            }
 
             if (md5_file($pathToVersions.$file) == $_POST['checksum']) {
                 $response['version'] = $version;
@@ -26,6 +31,7 @@ if (isset($_POST['checksum'])) {
         $response['error'] = 1;
         $response['message'] = "Aucune version correspondante n'a été trouvée.";
     }
+
 } else {
     $response['error'] = 1;
     $response['message'] = "Checksum non fourni";
