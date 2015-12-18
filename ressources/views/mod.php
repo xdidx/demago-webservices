130b34a7d29dea951e3384f7c45f3c1ace707c37
DiD <rhandy.grard@gmail.com>
2015-12-18 11:31:23 +0100

    Add some informations & useful links for mod

--- ressources/views/mod.php
+++ ressources/views/mod.php
@@ -32,4 +32,34 @@
             </a>
         </div>
     </div>
+
+    <div class="col s12">
+        <div class="card">
+            <div class="card-content">
+                <div class="card-title black-text">
+                    Informations
+                </div>
+                Grâce à un outil nommé ScriptHook, nous avons la possibilité de créer des librairies de code (DLL) à ajouter dans le dossier du jeu et qui seront interprétées par ce dernier.
+                <br/>
+                Nous sommes ensuite libre de faire beaucoup de modifcations dans le jeu en fonction des actions de l'utilisateur.
+                <br/>
+                Le modding sur GTA V avec ScriptHook se fait en C++ mais le "framework" nommé ScriptHookDotNet nous permet de coder en C# avec des objets et des méthodes plus simples à comprendre bien que les developpements soient assez similiaires
+            </div>
+        </div>
+    </div>
+
+    <div class="col s12">
+        <div class="card">
+            <div class="card-content">
+                <div class="card-title black-text">
+                    Liens utiles
+                </div>
+                <a href="http://gtaforums.com/forum/370-gta-v/">Forums de code et de documentation sur le modding GTA V</a>
+                <br/>
+                <a href="http://gtaforums.com/topic/814258-c-ultimate-modding-thread//">Post ultime du mod C#</a>
+                <br/>
+                <a href="http://gtaforums.com/topic/803234-ultimate-modding-thread/">Post ultime du mod C++</a>
+            </div>
+        </div>
+    </div>
 </div>
